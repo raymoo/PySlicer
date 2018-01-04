667627f6fced2d1b69909457098c699777bb7f74
raymoo <uguu@installgentoo.com>
2018-01-04 09:25:08 -0800

    Write out jsonfile.sliced with exception slices added

--- process_json.py
+++ process_json.py
@@ -2,31 +2,16 @@ import make_trace
 import ast, sys, json
 import traceback
 
-DEBUG = False
+DEBUG = True
 decoder = json.JSONDecoder()
 
-class RemoveTransformer(ast.NodeTransformer):
-    def __init__(self, keep_set):
-        self.keep_set = keep_set
-
-    def visit(self, node):
-        if isinstance(node, ast.stmt):
-            if node in self.keep_set:
-                self.generic_visit(node)
-            else:
-                return ast.Pass()
-        return node
-
-def process_one(string):
+def process_one(outfile, string):
     obj = decoder.decode(string)
     source = obj['user_script']
     ri = json.dumps(obj['raw_input']) if 'raw_input' in obj else '[]'
 
     last_line = len(source.splitlines())
     slice_lines, slice_p = make_trace.slice(source, ri, debug=True)
-    astree = ast.parse(source)
-    visitor = RemoveTransformer(slice_lines)
-    astree = visitor.visit(astree)
 
     if slice_lines:
         print('Original code:')
@@ -34,18 +19,23 @@ def process_one(string):
 
         print('Lines to keep: ' + str(slice_lines))
         print('Line proportion removed: ' + str(slice_p))
+
+        obj['exception_slice'] = list(slice_lines)
     else:
         print("No exception")
 
-file = open(sys.argv[1])
+    json.dump(obj, outfile)
 
-for i, line in enumerate(file):
-    print('Interaction ' + str(i))
-    try:
-        process_one(line)
-    except Exception as e:
-        if DEBUG:
-            traceback.print_exc(None, sys.stdout)
-        else:
-            print(e)
-    print('')
+with open(sys.argv[1]) as infile:
+    with open(sys.argv[1] + '.sliced', 'wt') as outfile:
+
+        for i, line in enumerate(infile):
+            print('Interaction ' + str(i))
+            try:
+                process_one(outfile, line)
+            except Exception as e:
+                if DEBUG:
+                    traceback.print_exc(None, sys.stdout)
+                else:
+                    print(e)
+            print('')
